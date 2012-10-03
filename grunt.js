f5d7457fe61a0c4a3bf2ffad502285ba738eeb4c
JohnnyT <johnnyt@moneydesktop.com>
2012-10-02 19:36:37 -0600

    Blank page opening Amber IDE

--- grunt.js
+++ grunt.js
@@ -3,39 +3,90 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-exec');
 
   grunt.registerTask('default', 'amber concat');
+  grunt.registerTask('release', 'default min cssmin');
 
   grunt.initConfig({
     amber: {
-      'tmp/amber.compiled': [] // 'Cards', 'Presentation-MagLev' ]
+      'js/amber-compiled': []
     },
+
     concat: {
-      'public/js/app.js': [
-        'public/js/jquery-1.8.2.min.js',
+      'js/app.js': [
+        'js/jquery-1.8.2.min.js',
+
         // Amber IDE dependencies
-        'vendor/amber/js/lib/jQuery/jquery-ui-1.8.16.custom.min.js',
-        'vendor/amber/js/lib/jQuery/jquery.textarea.js',
-        'vendor/amber/js/lib/CodeMirror/codemirror.js',
-        'vendor/amber/js/lib/CodeMirror/smalltalk.js',
-        'tmp/amber.compiled.js'
+        'amber/js/lib/jQuery/jquery-ui-1.8.16.custom.min.js',
+        'amber/js/lib/jQuery/jquery.textarea.js',
+        'amber/js/lib/CodeMirror/codemirror.js',
+        'amber/js/lib/CodeMirror/smalltalk.js',
+
+        'js/amber-compiled.js'
       ],
-      'public/css/app.css': [
-        'public/css/bootstrap.min.css',
-        'public/css/bootstrap.responsive.min.css',
-        'vendor/amber/css/amber.css',
-        'vendor/amber/js/lib/CodeMirror/codemirror.css',
-        'vendor/amber/js/lib/CodeMirror/amber.css',
-        'public/css/amber-overrides.css'
+
+      // Amber IDE dependencies
+      'css/amber-concat.css': [
+        'amber/css/amber.css',
+        'amber/js/lib/CodeMirror/codemirror.css',
+        'amber/js/lib/CodeMirror/amber.css',
+        'css/amber-overrides.css'
+      ],
+
+      'css/app.css': [
+        'css/bootstrap.css',
+        'css/bootstrap.responsive.css',
+        'css/amber-concat.css'
+        // 'tmp/main.css'
       ]
+    },
+
+    cssmin: {
+      'css/app.min.css': ['css/app.css']
+    },
+
+    min: {
+      'js/app.min.js': ['js/app.js']
+    },
+
+    watch: {
+      files: ['js/*.deploy.js'],
+      tasks: 'default'
     }
   });
 
   grunt.registerMultiTask('amber', 'Compile Amber Smalltalk', function() {
-    var libraries = this.data.map(function(package){ return '../../../js/'+package; });
-    libraries = 'parser,Compiler,Canvas,IDE,SUnit,'+libraries;
-    var cmd = './vendor/amber/bin/amberc -l ' + libraries + ' ' + this.target;
-    grunt.log.writeln('Executing: ' + cmd);
-    grunt.helper('exec', cmd);
-    grunt.log.writeln('File "' + this.target + '.js" created.');
-  });
+    var userPackages = this.data.map(function(package){ return '../../js/'+package; });
 
+    var additionalPackages = [
+      'parser',
+      'Importer-Exporter',
+
+      'Compiler-Exceptions',
+      'Compiler-Core',
+      'Compiler-AST',
+      'Compiler-Semantic',
+      'Compiler-IR',
+      'Compiler-Inlining',
+      'Kernel-Announcements',
+
+      'SUnit',
+      'Compiler-Tests',
+      'Kernel-Tests',
+      'Canvas',
+      'IDE',
+
+      'compat'
+    ];
+
+    var allPackages = additionalPackages.join(',');
+    if (userPackages.length > 0) {
+      allPackages += ',' + userPackages.join(',');
+    }
+
+    var targetFile = this.target;
+    var cmd = './amber/bin/amberc -l ' + allPackages + ' ' + targetFile;
+    grunt.log.writeln('Executing: ' + cmd);
+    grunt.helper('exec', cmd, function(){
+      grunt.log.writeln('File "' + targetFile + '.js" created.');
+    });
+  });
 };
---
+++ server.js
@@ -0,0 +1,1 @@
+amber/server/server.js
